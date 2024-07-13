b0bbbbae32552cc22c96a98a1523b4a81d6aee10
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-07-13 21:27:40 +0530

    Variables and Data Types

---
+++ Day 1 Variables and Data Types/index.js
@@ -0,0 +1,44 @@
+// Activity 1: variable Declaration
+var val = 11;
+console.log(val);
+
+let str = "lokesh singh";
+console.log(str);
+
+
+// Activity 2: Constant Declaration
+const isValid = true;
+console.log(isValid);
+
+
+// Activity 3: Data Types
+const num = 11;
+const fullname = "lokesh singh";
+const isCorrect= false;
+const obj = {
+    firstname: "lokesh",
+    lastname: "singh"
+}
+const skills = ["React", "node", "express", "mongodb"]
+
+console.log(typeof(num));
+console.log(typeof(fullname));
+console.log(typeof(isCorrect));
+console.log(typeof(obj));
+console.log(typeof(skills));
+
+
+// Activity 4: Reassigning variables
+let role = "frontend developer"
+console.log(role);
+
+role = "backend developer"
+console.log(role)
+
+
+// Activity 5: Understanding const
+const userId = "2024354";
+console.log(userId);
+
+userId = "20344357";
+console.log(userId);

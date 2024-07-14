40de47322293a9424ff739d09238de583e515b3d
Lokesh Singh <lokeshsingh0089@gmail.com>
2024-07-14 22:38:54 +0530

    Operators

---
+++ Day 2 Operators/index.js
@@ -0,0 +1,58 @@
+// Activity 1: Arithmetic operations
+const val1 = 10;
+const val2 = 20;
+
+const sum = val1 + val2;
+const sub = val1 - val2;
+const mul = val1 * val2;
+const div = val1 / val2;
+const rem = val1 % val2;
+
+console.log(sum);
+console.log(sub)
+console.log(mul);
+console.log(div);
+console.log(rem);
+
+
+// Activity 2: Assignment operators
+let a = 10;
+a += 1;
+console.log(a);
+
+let b = 10;
+b -= 1;
+console.log(b);
+
+
+// Activity 3: Comparison operators
+const num1 = 10;
+const num2 = 20;
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+const x = 10;
+const y = "10";
+console.log(x == y);
+console.log(x === y);
+
+
+// Activity 4: Logical operators
+const bool1 = true;
+const bool2 = false;
+
+if(bool1 && bool2)
+    console.log("Both are true");
+if(bool1 || bool2)
+    console.log("At least one is true");
+if(!bool2)
+    console.log("bool2 is false");
+
+
+// Activity 5: Ternary operators
+const res = 23 === "23" ? console.log("first condition is true") : console.log("first condition is false");
+
+
